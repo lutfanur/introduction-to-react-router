546a3d34d95333f4ca32b1ec13ef27b3befa5d35
Lutfaakter <lutfanur20@gmail.com>
2024-03-20 13:40:23 +0000

    added recap  make route dynamically and added error message

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,20 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const Post = ({ post }) => {
     const { id, title } = post;
+
+    const navigate = useNavigate();
+
     const userStyle = {
         border: '2px solid green',
         padding: '5px',
         borderRadius: '20px'
-
     }
+
+    const handleShowDetail = () => {
+        navigate(`/post/${id}`);
+    }
+
     return (
         <div style={userStyle}>
             <h4>Post of Id: {id}</h4>
             <p>{title}</p>
             <Link to={`/post/${id}`}>Post Detail</Link>
-            <Link to={`/post/${id}`}><button>Show Details</button></Link>
+            <Link to={`/post/${id}`}><button>Show Details</button></Link><br />
+            <button onClick={handleShowDetail}>Click to see details</button>    
         </div>
     );
 };
